55b58ae1d5785b511b71eea40fb792aaabe1663b
PedroByJoao <joaobk09@gmail.com>
2024-07-28 17:00:54 +0000

    fix(nvim): golang formatter mistype

--- hm/nvim/configs/formatting.lua
+++ hm/nvim/configs/formatting.lua
@@ -3,7 +3,7 @@ local conform = require("conform")
 conform.setup({
 	formatters_by_ft = {
 		nix = { "nixfmt", "nixpkgs_fmt" },
-		golang = { "gofumpt", "goimports-reviser" },
+		go = { "gofumpt", "goimports-reviser" },
 		lua = { "stylua" },
 		rust = { "rustfmt" },
 		python = { "black" },
