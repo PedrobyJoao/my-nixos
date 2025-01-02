cec6b45fcc3d7665b3f28c2d54a577a22da50ef7
PedroByJoao <joaoexplorer235@gmail.com>
2025-01-02 11:40:43 -0300

    maybe-fix: screen sharing hyprland

--- hosts/anathem/configuration.nix
+++ hosts/anathem/configuration.nix
@@ -83,8 +83,8 @@
   xdg.portal = {
     enable = true;
     extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-wlr
+      # xdg-desktop-portal-gtk
+      # xdg-desktop-portal-wlr
       xdg-desktop-portal-hyprland
     ];
   };
