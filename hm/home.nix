4de77f62592b3b2a69e50b405985b867bdca4e9f
PedroByJoao <joaoexplorer235@gmail.com>
2025-04-24 15:35:20 -0300

    feat: delta

--- hm/home.nix
+++ hm/home.nix
@@ -160,6 +160,14 @@
 
   programs.lazygit = {
     enable = true;
+    settings = {
+      git = {
+        paging = {
+          colorArg = "always";
+          pager = "delta --dark --paging=never";
+        };
+      };
+    };
   };
 
   gtk = {
--- hm/modules/git.nix
+++ hm/modules/git.nix
@@ -13,5 +13,18 @@ in
     };
 
     lfs.enable = true;
+
+    delta = {
+      enable = true;
+      options = {
+        decorations = {
+          commit-decoration-style = "bold yellow box ul";
+          file-decoration-style = "none";
+          file-style = "bold yellow ul";
+        };
+        features = "decorations";
+        whitespace-error-style = "22 reverse";
+      };
+    };
   };
 }
